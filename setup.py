dadfc6e9a8cf7c5e39b938674930f6830e2c2e5f
Justin R. Cutler <justin.r.cutler@gmail.com>
2017-03-16 22:35:39 -0400

    Create initial package skeleton

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aio',
+    version='0.0.0',
+    description='A minimal asynchronous I/O engine',
+    author='Justin R. Cutler',
+    author_email='justin.r.cutler@gmail.com',
+    packages=find_packages(),
+    python_requires='3.6',
+)
